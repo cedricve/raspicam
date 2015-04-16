ca21838c547febc58b5c05eb78c4368b455b79d5
Cédric Verstraeten <cedric.verstraeten@me.com>
2015-04-16 09:17:43 +0200

    Update raspicam_cv.cpp

--- src/raspicam_cv.cpp
+++ src/raspicam_cv.cpp
@@ -113,15 +113,6 @@ namespace raspicam {
                 return -1;//auto
             else return Scaler::scale (0,330000, 0,100, _impl->getShutterSpeed() )  ;
 	   break;
-        case CV_CAP_PROP_CONVERT_RGB :
-            return ( imgFormat==CV_8UC3 );
-        case CV_CAP_PROP_WHITE_BALANCE_RED_V:
-                    return _impl->getAWBG_red()*100;
-        break;
-
-        case CV_CAP_PROP_WHITE_BALANCE_BLUE_U:
-            return _impl->getAWBG_blue()*100;
-        break;
         default :
             return -1;
         };
@@ -180,25 +171,6 @@ namespace raspicam {
         case CV_CAP_PROP_CONVERT_RGB :
             imgFormat=CV_8UC3;
             break;
-        case CV_CAP_PROP_WHITE_BALANCE_RED_V:
-                if (value==0) _impl->setAWB(raspicam::RASPICAM_AWB_AUTO);
-                else  {
-                    int valblue=_impl->getAWBG_blue()*100;
-                     _impl->setAWB(raspicam::RASPICAM_AWB_OFF);
-                     _impl->setAWB_RB(value*100,valblue);
-                };
-        break;
-
-        case CV_CAP_PROP_WHITE_BALANCE_BLUE_U:
-                if (value==0) _impl->setAWB(raspicam::RASPICAM_AWB_AUTO);
-                else  {
-                    int valred=_impl->getAWBG_red()*100;
-                     _impl->setAWB(raspicam::RASPICAM_AWB_OFF);
-                     _impl->setAWB_RB(valred, value*100 );
-                };
-        break;
-
-//     case CV_CAP_PROP_WHITE_BALANCE :return _cam_impl->getAWB();
         default :
             return false;
         };
